356ccc4eb8df3fefcd367505e0e7418c951af2ac
diego <diego@Dell>
2013-09-07 18:08:55 -0300

    Teste beans for ubuntu!

---
+++ TrabalhoPraticoI/src/trabalhopraticoi/TrabalhoPraticoI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhopraticoi;
+
+/**
+ *
+ * @author diego
+ */
+public class TrabalhoPraticoI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String teste;
+    }
+}
