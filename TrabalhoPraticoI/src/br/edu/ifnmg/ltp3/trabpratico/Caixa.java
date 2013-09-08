85418634c90164c919bd4f7a5bd23f54ae1414b3
diego <diego@Dell>
2013-09-08 13:49:21 -0300

    adicionado data em vendas.

---
+++ TrabalhoPraticoI/src/br/edu/ifnmg/ltp3/trabpratico/Caixa.java
@@ -0,0 +1,92 @@
+
+package br.edu.ifnmg.ltp3.trabpratico;
+
+/**
+ *
+ * @author diego
+ */
+public class Caixa {
+    int codigo;
+    String data;
+    double saldo;
+    
+    //Constructor
+    
+    public Caixa() {
+    }
+    //Constructor
+    
+    public Caixa(int codigo, String data, double saldo) {
+        this.codigo = codigo;
+        this.data = data;
+        this.saldo = saldo;
+    }
+    
+    //Methods
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    //Equal and HashCode
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Caixa other = (Caixa) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        if ((this.data == null) ? (other.data != null) : !this.data.equals(other.data)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.saldo) != Double.doubleToLongBits(other.saldo)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + this.codigo;
+        hash = 67 * hash + (this.data != null ? this.data.hashCode() : 0);
+        hash = 67 * hash + (int) (Double.doubleToLongBits(this.saldo) ^ (Double.doubleToLongBits(this.saldo) >>> 32));
+        return hash;
+    }
+    
+    // ToString
+    
+    @Override
+    public String toString() {
+        return "Caixa{" + "codigo=" + codigo + ", data=" + data + ", saldo=" + saldo + '}';
+    }
+    
+    
+    
+}
